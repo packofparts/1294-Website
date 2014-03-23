fbed5a592f3634f061df21ba6b785a4e1b1454af
blueal <blueal22@hotmail.com>
2014-03-22 22:53:47 -0700

    Initial Commit

---
+++ includes/header.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <!--The title for the page is set by setting the $title variable in the PHP include statement on the corresponding page-->
+        <title><?php echo $title; ?></title>
+        <meta name="description" content="The Official Website of the Top Gun (FRC 1294) Located In Sammmamish Washington participating in FIRST Robotics">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="/css/bootstrap.css">
+        <style>
+            body {
+                padding-top: 0px;
+                padding-bottom: 20px;
+                /*background: #78866B;*/
+                background:  #eeeeee url('http://i610.photobucket.com/albums/tt185/ndruqltn/Camouflage/NavyBlueCamo.png');
+            }
+        </style>
+        <link rel="stylesheet" href="/css/bootstrap-theme.css">
+        <link rel="stylesheet" href="/css/main.css">
+        <?php
+            /*
+            *There might be some extra tags that need to be placed in the header,
+            *Any extra tags needed will be added by setting the $ExtraTags Variable right before the include statement
+            *on a webpage, first we check if there is a tag set, if it is, we add itthe extra tags to the header
+            */
+            if (!empty($ExtraTags)) {
+                echo $ExtraTags;
+            }
+
+            //Figures out what page your on for the top bar menu
+            // gets the current URI, remove the left / and then everything after the / on the right
+            $directory = explode('/', ltrim($_SERVER['REQUEST_URI'], '/'));
+
+            // loop through each directory, check against the known directories, and add class   
+            $directories = array("", "about","contact","media","calendar"); // "" is the homepage sense we are using clean URL's
+            foreach ($directories as $folder){
+                $active[$folder] = ($directory[0] == $folder)? "active":"noactive";
+            }
+        ?>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <!--<img alt="All Team members" class="headerimage" src="/img/Big_Header.JPG" width="1271px" />-->
+            <div class="container">
+                <div class="navbar-header">
+                    <!--Start of Mobile Only Buttons-->
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <!--End of Mobile Only Buttons-->
+                    <a class="navbar-brand" href="/"><strong>Top Gun Robotics</strong></a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="<?php echo $active['']?>"><a href="/">Home</a></li>
+                        <li class="dropdown <?php echo $active['about']?>">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">About <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li class="dropdown-header"><a href="/about/">About</a></li>
+                                <li class="divider"></li>
+                                <li><a href="/about/team/">The Team</a></li>
+                                <li><a href="/about/website">Website</a></li>
+                            </ul>
+                        </li>
+                        <li class="<?php echo $active['contact']?>"><a href="/contact">Contact</a></li>
+                        <li class="<?php echo $active['media']?>"><a href="/media">Media</a></li>
+                        <li class="<?php echo $active['calendar']?>"><a href="/media">Calendar</a></li>
+                        <li class="dropdown <?php echo $active['something']?>">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Nav header</li>
+                            <li><a href="#">Separated link</a></li>
+                            <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="navbar-form navbar-right">
+                    <a href="/donate"class="btn btn-success">Donate</a>
+                    </form>
+                </div><!--/.navbar-collapse -->
+            </div>
+        </div>
+        <div class="body-container" id="top">
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php    
+    /**
+    *The front page to 1294's website
+    *
+    *@author Alex Friedberg <blueal22@hotmail.com>
+    *@link https://github.com/blueal/FRC-Team-1294
+    */
+    
+    /*Use $ExtraTags to add additional scripts or CSS files for specific pages
+    *Use $title to set the title of the webpage
+    */
+    $ExtraTags='<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+    <script type="text/javascript" src="engine1/jquery.js"></script>';
+    $title='1294: Top Gun Robotics';
+    require'/includes/header.php'; 
+ ?>
+
+	<!-- Start WOWSlider.com BODY section -->
+	<!--<link rel="stylesheet" type="text/css" href="engine1/style.css" />-->
+    <!--<script type="text/javascript" src="engine1/jquery.js"></script>-->
+
+	<div id="wowslider-container1">
+	    <div class="ws_images">
+            <ul>
+                <li><img src="data1/images/big_header.jpg" alt="The Team" title="The Team" id="wows1_0"/></li>
+                <li><img src="data1/images/img_0131.jpg" alt="Professionialism" title="Professionialism" id="wows1_1"/></li>
+                <li><img src="data1/images/img_0158.jpg" alt="Precision and Execution" title="Precision and Execution" id="wows1_2"/></li>
+                <li><img src="data1/images/img_0206.jpg" alt="We strive for the best" title="We strive for the best" id="wows1_3"/>Even if it means destroying the competition, we'll do it.</li>
+                <li><img src="data1/images/img_0217.jpg" alt="Collaboration" title="Collaboration" id="wows1_4"/></li>
+                <li><img src="data1/images/img_0226.jpg" alt="Speed and Efficency" title="Speed and Efficency" id="wows1_5"/></li>
+            </ul>
+        </div>
+        <div class="ws_bullets">
+            <div>
+                <a href="#" title="The Team"><img src="data1/tooltips/big_header.jpg" alt="The Team"/>1</a>
+                <a href="#" title="Professionialism"><img src="data1/tooltips/img_0131.jpg" alt="Professionialism"/>2</a>
+                <a href="#" title="Precision and Execution"><img src="data1/tooltips/img_0158.jpg" alt="Precision and Execution"/>3</a>
+                <a href="#" title="We strive for the best"><img src="data1/tooltips/img_0206.jpg" alt="We strive for the best"/>4</a>
+                <a href="#" title="Collaboration"><img src="data1/tooltips/img_0217.jpg" alt="Collaboration"/>5</a>
+                <a href="#" title="Speed and Efficency"><img src="data1/tooltips/img_0226.jpg" alt="Speed and Efficency"/>6</a>
+            </div>
+        </div>
+        <span class="wsl"><a href="http://wowslider.com">Gallery Script</a> by WOWSlider.com v5.2</span>
+	    <div class="wdss_shadow"></div>
+	</div>
+	<script type="text/javascript" src="engine1/wowslider.js"></script>
+	<script type="text/javascript" src="engine1/script.js"></script>
+    <!--End of WOWSlider Body Section-->
+    </div>
+    <div class="body-container">
+    <!-- Main jumbotron-->
+    <div class="jumbotron">
+      <div class="container">
+        <img id="eastlake-logo"alt="Eastlake Logo" height="250px" src="/img/Eastlake-Logo.png"/>
+        <h1>We are Top Gun Robotics</h1>
+        <p>We are a High School Robotics club/team located at <a href="http://www.lwsd.org/school/ehs/Pages/default.aspx">Eastlake High School</a> in Sammamish Washington that has been playing in FRC since 2004. </p>
+        <p><a class="btn btn-primary btn-lg learn-more" href="/about/team">Learn more &raquo;</a></p>
+      </div>
+    </div>
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>News/Announcments</h2>
+          <p>INSERT RSS FEED HERE!</p>
+          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+       </div>
+        <div class="col-lg-4">
+          <h2>Facebook</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+        </div>
+      </div>
+    </div>
+<?php require'/includes/footer.php'; ?>
