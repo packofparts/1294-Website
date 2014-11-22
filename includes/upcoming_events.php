bdf461616b82d9a95a35af6519533a1d6975f88f
Austin Jenchi <ajenchi9+code@gmail.com>
2014-11-22 13:58:27 -0800

    Add google-apis and do more work

--- includes/upcoming_events.php
+++ includes/upcoming_events.php
@@ -15,39 +15,39 @@
     as the previous version, but it gets the job done.
     */
 
-    $apiKey = 'AIzaSyDfxQCRwk1MkTHpmNAjL3xFl8CncA_MvD4'; // Your API Key Would Go Here
+    $apiKey = 'AIzaSyDbVDRAzrz64cO98PU7yTm-yEHtQ5ZN_8w'; // Your API Key Would Go Here
     // Your calendar id would go here, it can be found in your calendar details.
-    // It is encoded so that is can be used properly in the URL, so it's fine to use
-    // symbols like the @ sign.
-    $calendarId = urlencode('frc1294@gmail.com');
-    $apiUrl = 'https://www.googleapis.com/calendar/v3/calendars/'
-        .$calendarId
-        .'/events?key='
-        .$apiKey; // Concatanate all of the parameters into one URL.
+    $calendarId = 'frc1294@gmail.com';
 
     $cache = true; // Whether or not to cache and use cache files.
     $cacheFilePath = $_SERVER['DOCUMENT_ROOT'].'/cache/gcal.json';
 
-    if($cache){
-        $timedif = @(time() - filemtime($cacheFilePath));
-        if(file_exists($cacheFilePath) && $timedif < 43200 /* 12 hours in seconds */){
-            $data = file_get_contents($cacheFilePath);
-        }else{
-            getDataFromApi();
-        }
+    $data;
+
+    $timedif = @(time() - filemtime($cacheFilePath));
+    if($cache && file_exists($cacheFilePath) && $timedif < 43200 /* 12 hours in seconds */){
+        $data = json_decode(file_get_contents($cacheFilePath));
     }else{
         getDataFromApi();
     }
-    $data = json_decode($data);
-    echo $data;
-
+    echo json_encode($data);
 
     function getDataFromApi(){
-        $curl = curl_init($apiUrl);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $data = curl_exec($curl);
-        curl_close($curl);
-        //if($cache){file_put_contents($cacheFilePath, $data);}
+        try{
+            global $apiKey, $apiUrl, $calendarId, $data, $cache, $cacheFilePath;
+            error_reporting(-1);
+            ini_set('display_errors', 'On');
+            set_include_path(get_include_path().PATH_SEPARATOR.$_SERVER['DOCUMENT_ROOT'].'/includes/vendor/google-apis-client/src');
+            require_once '/includes/vendor/google-apis-client/autoload.php';
+            $client = new Google_Client();
+            $client->setDeveloperKey($apiKey);
+            $client->addScope('https://www.googleapis.com/auth/calendar.readonly');
+            $service = new Google_Service_Calendar($client);
+            $data = $service->events->listEvents($calendarId);
+            if($cache){file_put_contents($cacheFilePath, json_encode($data));}
+        }catch(Exception $e){
+            echo "Sorry, we couldn't get data from the calendar. Please click the button bellow to look at our full calendar.";
+        }
     }
 ?>
 </div>
