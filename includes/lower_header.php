02ff24c9f4d5c5e398748aef692f50ec729e8b9e
blueal <blueal22@hotmail.com>
2015-02-08 22:49:49 -0800

    Starting addition for competitions page
    Added new documents and started new 2015 competitions page

--- includes/lower_header.php
+++ includes/lower_header.php
@@ -86,11 +86,12 @@
                             <li class="dropdown <?php echo $active['about']?>">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-question-sign"></span> Information <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
-                                    <li><a href="/about/team/"><i class="fa fa-users"></i> Our Team</a></li>
-                                    <li><a href="/about/website/"><span class="glyphicon glyphicon-globe"></span> The Website</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="/about/joining/"><span class="glyphicon glyphicon-heart-empty"></span> Join Us!</a></li>
-                                    <li><a href="/about/donate/"><i class="fa fa-money"></i> Donations</a></li>
+                                    <li><a href="/about/team/"><i class="fa fa-connectdevelop"></i> Competitions</a></li>
+                                    <li style="display: none"><a href="/about/team/"><i class="fa fa-users"></i> Our Team</a></li>
+                                    <li style="display: none"><a href="/about/website/"><span class="glyphicon glyphicon-globe"></span> The Website</a></li>
+                                    <li style="display: none" class="divider"></li>
+                                    <li style="display: none"><a href="/about/joining/"><span class="glyphicon glyphicon-heart-empty"></span> Join Us!</a></li>
+                                    <li style="display: none"><a href="/about/donate/"><i class="fa fa-money"></i> Donations</a></li>
                                 </ul>
                             </li>
                         <?php } //End of preview only block ?>
