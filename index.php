cc48d4d0bad37801a2bcf53e8bacdb8cf5bd2bff
Austin Jenchi <timtim17@users.noreply.github.com>
2015-12-02 20:11:54 -0800

    Re-capitalize the M in members

--- index.php
+++ index.php
@@ -184,7 +184,7 @@
                 <h3 class="section-header">Past / Existing Memebers</h3>
                 <hr class="section-header-hr">
                 <p class="center-content">
-                    Past members are still required to turn in safety form and pass safety training, as well as complete STIMS.<br>
+                    Past Members are still required to turn in safety form and pass safety training, as well as complete STIMS.<br>
                 </p>
                 <div>
                     <ul class="no-bullets">
