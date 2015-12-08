f1ca0da615c4faefa55cba1bd3446bfd71bd96ce
gnarlyharly <mhjors@gmail.com>
2015-12-07 20:02:55 -0800

    Revert "Added a STIMS link to the forms section"
    This reverts commit 4a76b46381450772cb151177bd7cd6b5bf7880dc.

--- about/forms/2016.php
+++ about/forms/2016.php
@@ -26,8 +26,6 @@
                         <ul class="no-bullets">
                             <li><i class="fa fa-file-word-o icon-bullet"></i><a href="/documents/2016/EHS_Robotics_Info_Sheet_2015-2016_ver1.docx">2016 Robotics Info Sheet</a></li>
                             <li><i class="fa fa-file-pdf-o icon-bullet"></i><a href="/documents/EHS_Robotics_Parent_Permission_Safety_Letter_ver1.pdf">Required Permission/Safety Form</a></li>
-                            <li><i class="fa fa-file-pdf-o icon-bullet"></i><a href="https://my.usfirst.org/stims/Login.aspx">STIMS Registration</a></li>
-
                         </ul>
                         <ul class="no-bullets">
                             <li><i class="fa fa-pencil-square-o icon-bullet"></i><a href="https://docs.google.com/a/team1294.org/forms/d/1e2LtHBRZiF_DzdaH4S2ZOcQ5QGcEYKexqQW1R8Hwx3k/viewform">Official Member Signup Form</a></li>
