82fabfc8a3ed09e109806c5cbf082e94a8c19ee5
Alex Friedberg <blueal22@hotmail.com>
2016-02-19 08:29:21 -0800

    Added t-shirt order form to frontpage
    #50

--- index.php
+++ index.php
@@ -174,7 +174,8 @@
                     </p>
                     <div class="center-content center-text">
                         <div class="btn-group btn-group-justified">
-                            <a class="btn btn-primary" href="https://docs.google.com/a/team1294.org/forms/d/1e2LtHBRZiF_DzdaH4S2ZOcQ5QGcEYKexqQW1R8Hwx3k/viewform"><span class="fa fa-file-text-o"></span> Official Member Signup Form</a>
+                            <a class="btn btn-primary" href="https://docs.google.com/a/team1294.org/forms/d/1e2LtHBRZiF_DzdaH4S2ZOcQ5QGcEYKexqQW1R8Hwx3k/viewform"><i class="fa fa-file-text-o"></i> Member Signup Form</a>
+			    <a class="btn btn-primary" href="https://docs.google.com/a/team1294.org/forms/d/1RS7h-h0rUN2yARcoIo133ww9HzDfd-zKQOgIIqIGLzM/viewform"><i class="fa fa-fighter-jet icon-bullet"></i><a "> T-Shirt Order Form</a>
                         </div>
                     </div>
                 </div>
