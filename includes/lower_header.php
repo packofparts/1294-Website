98068ae94264dd38f333d560c3f1040ca8356039
Alex Friedberg <blueal22@hotmail.com>
2016-03-17 19:09:10 -0700

    Fixed relative urls for WOWSlider

--- includes/lower_header.php
+++ includes/lower_header.php
@@ -6,7 +6,7 @@
             }
         </style>
         <!--link rel="stylesheet" type="text/css" href="/subtrees/bootstrap/dist/css/bootstrap.min.css"-->
-        <link rel="stylesheet" type="text/css" href="/style/3-7-16.1.combined.main.css">
+        <link rel="stylesheet" type="text/css" href="/style/4-17-16.1.combined.main.css">
 
 
         <?php
