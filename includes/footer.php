b753506444b792bb585ae7e222ea80c0f5661fc2
Austin Jenchi <ajenchi9@gmail.com>
2017-01-02 09:01:33 -0800

    Sponsor carousel (#96)
    * working toward issue #63
    
    I would love it if someone could go over these to make sure all the
    sponsor logos are included before I add a carousel.  "EHS Robotics
    Familly Member" still needs to be included because they to not have a
    logo.
    
    * reformatting images
    
    changing image size so that the carousel looks uniform
    
    * added sponsor carousel
    
    Added a carousel for the sponsors in the footer but now there is some
    white space that needs to be filled.  Also, when either arrow is pressed
    it goes to the top of the page, I may add an issue for this.
    
    * sponsor update that was meant to be in the last commit
    
    footer part of the sponsor carousel for more info read previous commit
    
    * fixing the buttons on the sponsor carousel
    
    * fixing the sponsor carousel buttons
    
    fixing the carousel buttons and the GoDaddy typo
    
    * updated and resized logos
    
    small edits
    
    * fixing the shadow in the buttons
    
    * update
    
    commit fix
    
    * merge fix still
    
    still merge fixing
    
    * last commit to fix the merge
    
    * removing unneeded code
    
    * image updates
    
    * updating alt attributes
    
    updating alt attributes to reflect their true purpose
    
    * conflict resolution
    
    * recomended changes
    
    I removed the import that was not being used, changed the button colors
    to gray, and change the type in the ospi alt atribute.
    
    * working toward desktop only slider
    
    * test for the desktop sponsor slides
    
    I still need to make it smaller vertically and space the logos out more
    but other than that I am pretty happy with it.
    
    * slides for desktop complete
    
    * switched it from css to javascript
    
    Now it should only switch on mobile rather than checking the window
    size, it is hard for me to test this though.
    
    * Resolve merge conflicts, remove leftover merge files, and replace JS form factor recognition with Bootstrap .hidden classes
    
    * Change bootstrap classes for bootstrap 3 and modify animation
    
    * Update animation and separate the individual images
    
    * Delete old sponsor images
    
    * Fix play/pause button for sponsor carousel
    
    * Slow down sponsor carousel animation

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,150 @@
-        <div class="body-container footer" style="">
+<div class="body-container footer" style="overflow: hidden;">
             <footer class="container">
                 <div class="row">
                     <h2 id="sponsor-title">At Top Gun Robotics, We Love Our Sponsors!</h2><br>
                     <hr style="margin-top: -7px; ">
                 </div>
-                
-                <div class="row" class="sponsors">
-                    <div class="col-sm-4">
-                         <img class="img-responsive center-block" id="sponsor-kenworth" alt="Kenworth Logo" src="http://nctrucking.wildapricot.org/Resources/Pictures/Kenworth%20Bar%20Logo%20with%20A%20PACCAR%20Company%20Black%20Trans%20Background%20800%20DPIpng.png"/>
+                <style>
+                    .left, .right {
+                        background: none !important;
+                        outline: 0;
+                      color: darkslategrey;
+                    }
+
+                    .tech-slideshow {
+                        height: 300px;
+                        width: 6700px;
+                        /*margin: 0 auto;*/
+                        position: relative;
+                        overflow: hidden;
+                        transform: translate3d(0, 0, 0);
+                      text-align: initial;
+                    }
+
+                  .tech-slideshow img {
+                    max-height: 300px;
+                  }
+
+                  /* https://designshack.net/articles/css/infinitephotobanner/ */
+                  .tech-slideshow img.sponsor-first {
+                    animation: bannermove 60s linear infinite;
+                  }
+
+                  @keyframes bannermove {
+                    0% {
+                      margin-left: 0px;
+                    }
+
+                    100% {
+                      margin-left: -5249px;
+                    }
+                  }
+
+                  div#carouselSponsor ol.carousel-indicators#sp-carousel-indicators li {
+                    border-color: darkslategrey;
+                  }
+
+                  div#carouselSponsor ol.carousel-indicators#sp-carousel-indicators li.active {
+                    background-color: darkslategrey;
+                  }
+
+
+                </style>
+
+
+
+                <div id="carouselSponsor" class="carousel slide hidden-sm hidden-md hidden-lg" data-ride="carousel" style="width: 70%; height: 60%; margin: 0 auto">
+                    <ol class="carousel-indicators" id="sp-carousel-indicators">
+                        <li data-target="#carouselSponsor" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="1"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="2"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="3"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="4"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="5"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="6"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="7"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="8"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="9"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="10"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="11"></li>
+                        <li data-target="#carouselSponsor" data-slide-to="12"></li>
+                    </ol>
+
+                    <div class="carousel-inner" role="listbox">
+                         <div class="item active">
+                            <img src="/img/sponsors/ehs.jpg" alt="Eastlake High School Logo" title="Eastlake High School"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/google.jpg" alt="Google Logo" title="Google"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/lwsd.jpg" alt="Lake Washington School Foundation Logo" title="Lake Washington School Foundation"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/boeing.jpg" alt="Boeing Logo" title="Boeing"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/chase.jpg" alt="Chase Logo" title="Chase"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/FIRSTWa.jpg" alt="FIRST Washington Logo" title="FIRST Washington"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/godaddy.jpg" alt="GoDaddy Logo" title="GoDaddy"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/honeywell.jpg" alt="Honeywell Logo" title="Honeywell"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/kenworth.jpg" alt="Kenworth Logo" title="Kenworth"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/microsoft.jpg" alt="Microsoft Logo" title="Microsoft"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/ospi.jpg" alt="Office of the Superintendant of Public Instruction Logo" title="Office of the Superintendent of Public Instruction"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/rocketdyne.jpg" alt="Aerojet Rocketdyne Logo" title="Aerojet Rocketdyne"/>
+                        </div>
+                        <div class="item">
+                            <img src="/img/sponsors/sae.jpg" alt="SAE Washington Logo" title="SAE Washington"/>
+                        </div>
                     </div>
-                    <div class="col-sm-4">                        
-                       <img class="img-responsive center-block" id="sponsor-SAE" alt="SAE Northwest Logo" src="http://www.sae.org/images/sections/ms049/NWL_380839334_sae_nw_logo_smaller.gif"/>
-                    </div>
-                    <div class="col-sm-4">
-                        <img class="img-responsive center-block" id="sponsor-boeing" alt="Boeing Logo" src="http://www.iptv.org/medialib/media/Boeing_RGB_blue_std_P.JPG"/>
-                    </div>
-                    <hr>
+
+                    <a href="#carouselSponsor" role="button" id="playpause" onClick="toggleSponsorCarousel()">
+                        <span class="glyphicon glyphicon-pause" aria-hidden="true" id="sp-carousel-pause"></span>
+                        <span class="glyphicon glyphicon-play" aria-hidden="true" id="sp-carousel-play" style="display: none;"></span>
+                    </a>
+                    <a class="left carousel-control" href="#carouselSponsor" role="button" data-slide="next" onclick="$('#carouselSponsor').carousel('prev');">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carouselSponsor" role="button" data-slide="next" onclick="$('#carouselSponsor').carousel('next');">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
+
+                <div class="tech-slideshow hidden-xs" id="slideshow">
+                  <img class="sponsor-first" src="/img/sponsors/ehs.jpg" alt="Eastlake High School Logo" title="Eastlake High School"/>
+                  <img src="/img/sponsors/google.jpg" alt="Google Logo" title="Google"/>
+                  <img src="/img/sponsors/lwsd.jpg" alt="Lake Washington School Foundation Logo" title="Lake Washington School Foundation"/>
+                  <img src="/img/sponsors/boeing.jpg" alt="Boeing Logo" title="Boeing"/>
+                  <img src="/img/sponsors/chase.jpg" alt="Chase Logo" title="Chase"/>
+                  <img src="/img/sponsors/FIRSTWa.jpg" alt="FIRST Washington Logo" title="FIRST Washington"/>
+                  <img src="/img/sponsors/godaddy.jpg" alt="GoDaddy Logo" title="GoDaddy"/>
+                  <img src="/img/sponsors/honeywell.jpg" alt="Honeywell Logo" title="Honeywell"/>
+                  <img src="/img/sponsors/kenworth.jpg" alt="Kenworth Logo" title="Kenworth"/>
+                  <img src="/img/sponsors/microsoft.jpg" alt="Microsoft Logo" title="Microsoft"/>
+                  <img src="/img/sponsors/ospi.jpg" alt="Office of the Superintendant of Public Instruction Logo" title="Office of the Superintendent of Public Instruction"/>
+                  <img src="/img/sponsors/rocketdyne.jpg" alt="Aerojet Rocketdyne Logo" title="Aerojet Rocketdyne"/>
+                  <img src="/img/sponsors/sae.jpg" alt="SAE Washington Logo" title="SAE Washington"/>
+                  <img src="/img/sponsors/ehs.jpg" alt="Eastlake High School Logo" title="Eastlake High School"/>
+                  <img src="/img/sponsors/google.jpg" alt="Google Logo" title="Google"/>
+                  <img src="/img/sponsors/lwsd.jpg" alt="Lake Washington School Foundation Logo" title="Lake Washington School Foundation"/>
+                </div>
+
                 <div class="row">
                     <div class="col-sm-6 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">
                         <div class="">
@@ -26,6 +154,7 @@
                         </div>
                     </div>
                 </div>
+
             </footer>
         </div>
 
@@ -50,6 +179,24 @@
         ?>
 
         <script>
+            const $sponsorCarousel = $('#carouselSponsor');
+            const $sponsorPlay = $('#sp-carousel-play');
+            const $sponsorPause = $('#sp-carousel-pause');
+            let sponsorCarouselState = true; // false == paused
+
+            function toggleSponsorCarousel() {
+              sponsorCarouselState = !sponsorCarouselState;
+              if (sponsorCarouselState) {
+                $sponsorCarousel.carousel('cycle');
+                $sponsorPlay.hide();
+                $sponsorPause.show();
+              } else {
+                $sponsorCarousel.carousel('pause');
+                $sponsorPlay.show();
+                $sponsorPause.hide();
+              }
+            }
+
             <?php 
                 /*Set a different tracking code based on if your viewing this website on the beta version or the full version*/
                 if($preview){
