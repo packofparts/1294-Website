08fc4d4ec1aedcdfc96ed0fb4f275999ea2715b4
Austin Jenchi <ajenchi9@gmail.com>
2017-02-26 14:44:38 -0800

    Update livestream link and embed

--- includes/live_stream.php
+++ includes/live_stream.php
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <h2 id="live-stream-title">Watch the <em>FIRST®</em> STEAMworks Game Animation!</h2>
-                <p class="lead"><!-- We will be live streaming from the <a class="event-link" href="http://www.thebluealliance.com/event/2016wamou">PNW Mount Vernon District Event</a> on <br> -->
-<!--                 <span class="event-times">January 7th @ 7:00 AM PST</span> -->
+                <h2 id="live-stream-title">Watch Maverick compete at Auburn Mountainview!</h2>
+                <p class="lead">We will be live streaming from the <a class="event-link" href="http://www.thebluealliance.com/event/2017waamv">PNW Auburn Mountainview District Event</a> on <br>
+                 <span class="event-times">March 4th & 5th</span>
                 </p>
             </div>
         </div>
@@ -12,7 +12,7 @@
             <!--<div class="col-lg-10 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 col-lg-offset-1">-->
             <div class="col-xs-12">
                 <div class="embed-responsive embed-responsive-16by9" id="">
-                    <iframe id="Live-Stream" class="embed-responsive-item" src="https://www.youtube.com/embed/EMiNmJW7enI?list=PLZT9pIgNOV6bpbpEKQBVqIUG09i9Ai3jM" scrolling="no" frameborder="0" style="border: 0px none transparent;" seamless allowfullscreen="allowfullscreen"></iframe>
+                    <iframe id="Live-Stream" class="embed-responsive-item" src="http://www.ustream.tv/embed/20054416?v=3&wmode=opaque" scrolling="no" frameborder="0" style="border: 0px none transparent;" seamless allowfullscreen="allowfullscreen"></iframe>
                 </div>
             </div>
         </div>
@@ -21,7 +21,7 @@
                 <table id="stream-buttons">
                     <tbody>
                         <tr style="width:100%;">
-<!--                            --><?php
+<?php
 //                            /*
 //                            *The following code is to get rid of the calendar button when the event is in progress or when the event has passed
 //                            *We'll eventually be able to do this by checking the event database
@@ -39,44 +39,31 @@
 //                            $eventDate='18-03-2016';
 //                            if(strtotime($eventDate) > strtotime('today')){
 //                                /*Its not passed the event date, show the "before event" button*/ ?>
-<!--                            <td style="width: 50%;">-->
-<!--                                <a href="http://www.thebluealliance.com/event/2016wamou" title="Add to Calendar" class="addthisevent">-->
-<!--                                    Add to Calendar-->
-<!--                                    <span class="_start">19-03-2016 9:00:00</span>-->
-<!--                                    <span class="_end">20-03-2016 17:00:00</span>-->
-<!--                                    <span class="_zonecode">6</span>-->
-<!--                                    <span class="_summary">Mount Vernon Competition</span>-->
-<!--                                    <span class="_description">PNW Mount Vernon District Competition</span>-->
-<!--                                    <span class="_location">314 N 9th Street Mount Vernon, WA 98273</span>-->
-<!--                                    <span class="_organizer">Washington FIRST Robotics</span>-->
-<!--                                    <span class="_organizer_email">Organizer e-mail</span>-->
-<!--                                    <span class="_all_day_event">false</span>-->
-<!--                                    <span class="_date_format">DD/MM/YYYY</span>-->
-<!--                                </a>-->
-<!--                            </td>-->
-<!--                            <td style="width: 50%;">-->
-<!--                                <a href="https://www.thebluealliance.com/event/2016wamou/" target="_blank" title="Go to Team List" class="go-to-scores" >-->
-<!--                                    <span class="glyphicon glyphicon-list-alt"></span> Get Team List-->
-<!--                                </a>-->
-<!--                            </td>-->
-<!--                            --><?php
+                            <td style="width: 50%;">
+                                <a href="http://www.thebluealliance.com/event/2016wamou" title="Add to Calendar" class="addthisevent">
+                                    Add to Calendar
+                                    <span class="_start">04-03-2017 8:15:00</span>
+                                    <span class="_end">05-03-2017 17:30:00</span>
+                                    <span class="_zonecode">6</span>
+                                    <span class="_summary"><em>FIRST</em> Washington: Auburn Mountainview District Event</span>
+                                    <span class="_description"><em>FIRST</em> Washington: Auburn Mountainview District Event</span>
+                                    <span class="_location">28900 124th Ave SE, Auburn, WA 98092</span>
+                                    <span class="_organizer"><em>FIRST</em> Washington</span>
+                                    <span class="_organizer_email">admin@firstwa.org</span>
+                                    <span class="_all_day_event">false</span>
+                                    <span class="_date_format">DD/MM/YYYY</span>
+                                </a>
+                            </td>
+                            <td style="width: 50%;">
+                                <a href="https://www.thebluealliance.com/event/2017waamv/" target="_blank" title="Go to The Blue Alliance" class="go-to-scores" >
+                                    <span class="glyphicon glyphicon-list-alt"></span> More Information at The Blue Alliance
+                                </a>
+                            </td>
+                            <?php
 //                            }
-//                            else
+                            //else
 //                            {
 //                                /*It has passed the event date, the event may be live streaming right now so show the correct buttons accordingly*/
-//                            ?>
-                            <td>
-                                <a href="https://www.youtube.com/playlist?list=PLZT9pIgNOV6bpbpEKQBVqIUG09i9Ai3jM" data-placement="top" data-toggle="tooltip" title="Watch all of the Kickoff videos on YouTube" class="add-tooltip btn btn-default">
-                                    <span class="fa fa-youtube-play" aria-hidden="true"></span> <em>FIRST</em> STEAMworks Kickoff Playlist
-                                    <!--Well eventually get the link through the database and do everything automatically-->
-                                </a>
-                            </td>
-                            <td>
-                                <a href="http://www.firstinspires.org/resource-library/frc/competition-manual-qa-system" target="_blank" class="add-tooltip btn btn-default" data-toggle="tooltip" data-placement="top" data-title="Click here for more information about FIRST STEAMworks"  >
-                                    <span class="fa fa-book" aria-hidden="true"></span> Game &amp; Season Information
-                                </a>
-                            </td>
-<!--                            --><?php
 //                            }
 //                            /*End of PHP for date checking, well evetually do everything automatically with the database at some point*/
 //                            ?>
